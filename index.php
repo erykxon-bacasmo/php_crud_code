912352812b430f1981ad89b46c478b88afb73a29
Erykxon Bacasmo <114896787+erykxon@users.noreply.github.com>
2023-05-25 22:58:17 +0800

    I add bootstrap in data table

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sample PHP CRUD</title>
     <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
 </head>
 <body>
     <!-- For PHP Add function -->
@@ -88,7 +90,7 @@ $result = $conn->query($sql);
 
 
     <!-- Table Section -->
-    <table class="table">
+    <table id="example" class="table table-striped" style="width: 80%; margin: auto;">
         <thead>
             <tr>
                 <th>Full Name</th>
@@ -120,6 +122,9 @@ $result = $conn->query($sql);
             ?>    
         </tbody>
     </table>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
     <script src="script.js"></script>
 </body>
 </html>
